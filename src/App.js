9a24f371aa66c0ce9d52fa3433ab8a2975ecaf6e
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-05-11 14:13:15 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hi</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
