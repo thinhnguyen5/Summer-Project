3a5315933f84d24098ca365658432484adedb98a
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-05-17 14:02:00 +0300

    Sign in form fuction

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FromSignUp from './FromSignUp'
+
+const Form = () => {
+    return (
+        <div>
+            <FromSignUp/>
+        </div>
+    )
+}
+
+export default Form
+
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import Form from '../Form';
 
 
 export default function SignUp() {
     return (
+        <>
+        <Form/>
         <Footer/>
+        </>
     )
     
 }
