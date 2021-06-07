aae3b77f4446f63df0264fe819201d54d5d545df
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-06-07 06:32:57 +0300

    add google map

--- src/components/pages/Location.js
+++ src/components/pages/Location.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import '../../App.css';
-import Footer from '../Footer';
+import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
+// import Footer from '../Footer';
+
+function Map() {
+    return (
+        <GoogleMap 
+            defaultZoom={10} 
+            defaultCenter={{ lat: 21.027763, lng: 105.834160}} 
+        />
+    );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function Location() {
     return (
-    <Footer/>
-    )
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <MapWrapped
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+            process.env.REACT_APP_GOOGLE_KEY
+          }`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
 }
